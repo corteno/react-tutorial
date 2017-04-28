96f9eed7354ef863c37e54d31c35227868f8d59a
Dávid <corten.mgs@gmail.com>
2017-04-28 13:53:53 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDKSHOjEWO3fWq5MWLrJmavVJd7MucgtuQ';
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+};
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
